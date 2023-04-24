de57ea397e03e27f24e6d806ff3163a78aece600
Akzhol <cheder911@gmail.com>
2023-04-24 23:32:20 +0600

    1

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int greatest;
+        greatest = first>second?
+                first:
+                second>third?
+                        second:
+                        third>first?
+                            third:
+                            first;
+        System.out.println(greatest);
+
     }
 }
